34cf4c90d82bcadd4ac4004b5ec650d3a87bd901
akshayk-7 <akshaykokitkar803@gmail.com>
2026-07-04 22:44:50 +0530

    Added the basic welcome message code

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 #!/usr/bin/env node
-console.log("Testing our CLI!!");
+
+import pc from 'picocolors';
+import figlet from 'figlet';
+
+// Generating the massive ASCII text
+const asciiArt = figlet.textSync('Wow CLI', {
+    font: 'Standard',
+    horizontalLayout: 'default',
+    verticalLayout: 'default'
+});
+
+// now adding some color to the ASCII art
+const coloredArt = pc.cyan(asciiArt);
+
+console.log(coloredArt);
+
+// printing the colorful welcome message
+console.log(pc.blueBright('Welcome to the First CLI tool ! \n'));
+
+
